26b093384770efea18fbe31d232a9a1c57168977
yash223t <yash.223yat@gmail.com>
2026-05-29 20:19:54 +0530

    Update calculatorgui.java

--- calculatorgui.java
+++ calculatorgui.java
@@ -1,1 +1,18 @@
+questions = {
+    "Capital of India": "Delhi",
+    "5 + 5": "10",
+    "Python creator": "Guido"
+}
 
+score = 0
+
+for question, answer in questions.items():
+    user_answer = input(question + ": ")
+
+    if user_answer == answer:
+        print("Correct!")
+        score += 1
+    else:
+        print("Wrong!")
+
+print("Final Score:", score)
